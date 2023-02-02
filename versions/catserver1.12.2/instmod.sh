62766437e4a419f73b31a7c12b69b918c32ab746
misaka12007 <misaka12007@outlook.com>
2023-02-02 10:16:42 +0000

    Thu 02 Feb 2023 10:16:42 AM UTC

---
+++ versions/catserver1.12.2/instmod.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ./mods
+wget https://github.com/misaka12007/CreeperThaumaturge/releases/download/mods/mods.tar
+tar -xvf mods.tar && ls -1 && echo "--- mods installation finished ---"
